9639bbc587f3e6c4f67521b88ef416ad10f3d6dd
MThVisStud Coll <MORGANTHOMAS2@exe-coll.ac.uk>
2025-02-11 14:42:40 +0000

    Altered Model to correctly define itself with the correct context

--- api/models/user_model.py
+++ api/models/user_model.py
@@ -1,24 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 class User:
 
-    def __init__(self, db, username, email, password):
+    def __init__(self, app, db):
+        self.app : Flask = app
         self.db : SQLAlchemy() = db
-        self.username = username
-        self.email = email
-        self.password = password
-        self.id = None
 
-    def create_table(self):
-        db = self.db
+    def defineModel(self):
+
         class UserModel(self.db.Model):
             __tablename__ = 'users'
-            id = db.Column(db.Integer, primary_key=True)
-            username = db.Column(db.String, nullable=False)
-            email = db.Column(db.String, unique=True, nullable=False)
-            password = db.Column(db.String, nullable=False)
+            id = self.db.Column(self.db.Integer, primary_key=True)
+            username = self.db.Column(self.db.String, nullable=False)
+            email = self.db.Column(self.db.String, unique=True, nullable=False)
+            password = self.db.Column(self.db.String, nullable=False)
 
-            def __init__(self, username, email, password):
+            def __init__(self, id, username, email, password):
+                self.id = id
                 self.username = username
                 self.email = email
                 self.password = password
@@ -26,4 +25,4 @@ class User:
             def __repr__(self):
                 return f'<User {self.username}>'
 
-        db.create_all()
+        return UserModel
