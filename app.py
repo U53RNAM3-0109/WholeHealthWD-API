ac92851970991fb1182a525bbc3fde154a4d2f9e
MThVisStud Coll <MORGANTHOMAS2@exe-coll.ac.uk>
2025-02-11 14:43:32 +0000

    Altered run script to correctly launch the Flask App

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from api import api
-from config import KeyVaultManager
+#from api import api
+#from config import KeyVaultManager
 import os
 
+from api.api import BtecBytesAPI
 
-app = api.BtecBytesAPI
+app = BtecBytesAPI(__name__, 'sqlite:///test.db', None)
+app.run(debug=True)
