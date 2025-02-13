b47bcefb5699b596d0d51ca103cc167aad73ee45
PyhcarmU53RNAM3-0109Home <MorganThomas2@exe-coll.ac.uk>
2025-02-13 11:13:03 +0000

    Exchanged username for first/lastname

--- api/models/user_model.py
+++ api/models/user_model.py
@@ -2,32 +2,35 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from api.models import common_model_addons as cmn
 
-class User:
 
+class User:
     def __init__(self, app, db):
-        self.app : Flask = app
-        self.db : SQLAlchemy() = db
+        self.app: Flask = app
+        self.db: SQLAlchemy() = db
 
     def define_model(self):
         class UserModel(self.db.Model, cmn.BaseIDandTableName, cmn.TimestampCreatedMixin, cmn.TimestampLastEditMixin):
-            username = self.db.Column(self.db.String, nullable=False)
+            firstname = self.db.Column(self.db.String, nullable=False)
+            lastname = self.db.Column(self.db.String, nullable=False)
             email = self.db.Column(self.db.String, unique=True, nullable=False)
             password = self.db.Column(self.db.String, nullable=False)
 
-            def __init__(self, username, email, password):
+            def __init__(self, firstname, lastname, email, password):
                 super().__init__()
-                self.username = username
+                self.firstname = firstname
+                self.lastname = lastname
                 self.email = email
                 self.password = password
 
             def to_dict(self):
-                #TODO: Move JSON Serialisation related functions to use Marshmallow Schemas
-                data = {'id':self.id,
-                        'username':self.username,
-                        'email':self.email,
-                        'password':self.password,
-                        'created_at':str(self.created_at),
-                        'last_edit':str(self.last_edit)}
+                # TODO: Move JSON Serialisation related functions to use Marshmallow Schemas
+                data = {'id': self.id,
+                        'firstname': self.firstname,
+                        'lastname': self.lastname,
+                        'email': self.email,
+                        'password': self.password,
+                        'created_at': str(self.created_at),
+                        'last_edit': str(self.last_edit)}
                 return data
 
         return UserModel
