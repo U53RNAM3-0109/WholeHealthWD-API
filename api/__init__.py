0767fa8c45e70485a0c2ff112a4646bad361cf63
PyhcarmU53RNAM3-0109Home <MorganThomas2@exe-coll.ac.uk>
2025-02-13 11:26:31 +0000

    Added new models and triggers for updating last_edit in user

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
 
-from .models import user_model
+from .models.user_model import User
+from .models.user_child_models.admin_model import Admin
+from .models.user_child_models.student_model import Student
+from .models.user_child_models.teacher_model import Teacher
+
+
 from .resources import user
 
 from flask import Flask
@@ -18,8 +24,8 @@ class BtecBytesAPI(Flask):
         self.db = SQLAlchemy()
         self.db.init_app(self)
 
-        UserModel = user_model.User(self, self.db)
-        self.UserModel = UserModel.define_model()
+        self.define_models()
+        self.register_triggers()
 
         with self.app_context():
             self.db.create_all()
@@ -29,3 +35,24 @@ class BtecBytesAPI(Flask):
 
         self.api.init_app(self)
 
+    def define_models(self):
+        self.UserModel = User(self, self.db).define_model()
+        self.AdminModel = Admin(self, self.db).define_model()
+        self.StudentModel = Student(self, self.db).define_model()
+        self.TeacherModel = Teacher(self, self.db).define_model()
+
+    def register_triggers(self):
+        def prevent_multiple_user_types(mapper, connection, target):
+            existing_user = self.db.session.connection().execute(
+                "SELECT COUNT(*) FROM admin WHERE user_id = ? UNION ALL "
+                "SELECT COUNT(*) FROM student WHERE user_id = ? UNION ALL "
+                "SELECT COUNT(*) FROM teacher WHERE user_id = ?",
+                (target.user_id, target.user_id, target.user_id)
+            ).fetchall()
+
+            if sum([row[0] for row in existing_user]) > 0:
+                raise Exception("Attempting to assign a User to multiple roles")
+
+        event.listen(self.AdminModel, 'before_insert', prevent_multiple_user_types)
+        event.listen(self.StudentModel, 'before_insert', prevent_multiple_user_types)
+        event.listen(self.TeacherModel, 'before_insert', prevent_multiple_user_types)
